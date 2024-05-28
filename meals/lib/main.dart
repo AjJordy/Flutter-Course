0cc79b7526372686a08e256e7832d85f4790cba2
AjJordy <jordyfaria0@gmail.com>
2024-05-28 09:51:23 -0300

    init meals project

---
+++ meals/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/utils/app_routes.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  final ThemeData theme = ThemeData();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.pink,
+          secondary: Colors.amber,
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+              ),
+              labelLarge: const TextStyle(
+                fontFamily: 'Raleway',
+              ),
+            ),
+      ),
+      home: const CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME: (ctx) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => const CategoriesMealsScreen(),
+      },
+    );
+  }
+}
